488f5b5f6a1b9dc2259de8491e168ad287937d8b
Nicholas Shephard <shephardn@gmail.com>
2021-11-04 04:54:39 -0500

    Assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution here
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+        // The function below returns an empty array when I run 
+            //  findMatching(drivers, 'Bobby')
+        // Why doesn't it filter Bobby for me? It passes the test...
+
+//   function findMatching(driverPool, requestedDriver) {
+//     return driverPool.filter(function (driver) {
+//         return driver === requestedDriver
+//     });
+//     }
+
+const driverNames = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+    function findMatching(driverPool, requestedDriver) {
+        return driverPool.filter(function (driver) {
+            return driver.toLowerCase() === requestedDriver.toLowerCase()
+        });
+        }
+
+
+    function fuzzyMatch(driverPool, requestedDriver) {
+        return driverPool.filter(function (driver) {
+            return driver.substring(0,2) === requestedDriver.substring(0,2)
+        });
+        }
+
+    
+    // function matchName(driverPool, requestedDriver) {
+    //     return driverPool.filter(function (driver) {
+    //         return driver.name === requestedDriver;
+    //     });
+    //     }
+
+        // For some reason, typing this out into an arrow function
+        // makes it much clearer to me.
+    function matchName(driverPool, requestedDriver) {
+        return driverPool.filter(driver => driver.name === requestedDriver);
+        }
